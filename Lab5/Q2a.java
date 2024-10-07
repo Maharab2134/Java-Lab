cd8cf7259437bbc45c5089d711479c2ee58bd1ec
Maharab Hosen <maharabhosen365@gmail.com>
2024-10-07 17:02:50 +0600

    Update 07-10-24

--- Lab5/Q2a.java
+++ Lab5/Q2a.java
@@ -1,11 +1,10 @@
 // Car and Bus are different types of vehicles. Car has its property speed and no_ of passengers. The Bus also has the same properties. Both the vehicle's speed depends on its engine and passengers depend on number of sits given when a Car or a Bus object is created. You are assigned to the task of comparing the two types of vehicles by their speed. Now translate the scenario to a java program, by creating the classes and comparing the speed of two objects of the classes Bus and Car. 
 
-
 class Vehicle {
     int speed;
     int no_of_passengers;
     
-    Vehicle(int speed, int no_of_passengers) {
+    Vehicle(int speed, int no_of_passengers) {// Constructor
         this.speed = speed;
         this.no_of_passengers = no_of_passengers;
     }
@@ -19,7 +18,7 @@ class Car extends Vehicle {
 
 class Bus extends Vehicle {
     Bus(int speed, int no_of_passengers) {
-        super(speed, no_of_passengers);
+        super(speed, no_of_passengers); //super is used to call the constructor of the parent class
     }
 }
 
---
+++ README.md
@@ -0,0 +1,3 @@
+# Java impotent some code
+## OOP
+
